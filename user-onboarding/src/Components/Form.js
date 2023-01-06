fec75e4b73c3600df81d0f4994d58a841c5aa9b3
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-06 11:40:14 -0500

    added classes and a submit button to Form.js

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -2,51 +2,61 @@ import React from 'react';
 
 const Form = (props) => {
     return (
-        <form>
-            <label>First Name 
-                <input
-                    type='text'
-                    name='first_name'
-                    placeholder='Enter First Name'
-                    value={values.first_name}
-                    onChange={onChange}
-                />
-            </label>
-            <label>Last Name 
-                <input
-                    type='text'
-                    name='last_name'
-                    placeholder='Enter Last Name'
-                    value={values.last_name}
-                    onChange={onChange}
-                />
-            </label>
-            <label>Email 
-                <input
-                    type='email'
-                    name='email'
-                    placeholder='Enter Email'
-                    value={values.email}
-                    onChange={onChange}
-                />
-            </label>
-            <label>Password 
-                <input
-                    type='password'
-                    name='password'
-                    placeholder='Type a Password'
-                    value={values.password}
-                    onChange={onChange}
-                />
-            </label>
-            <label>Terms of Service 
-                <input
-                    type='checkbox'
-                    name='terms'
-                    checked={values.terms}
-                    onChange={onChange}
-                />
-            </label>
+        <form className='form container' onSubmit={onSubmit}>
+            <div className='form submit'>
+                <h2>Add New User</h2>
+                <button disabled={disabled}>Submit</button>
+            </div>
+
+            <div className='form inputs'>
+                <label>First Name 
+                    <input
+                        type='text'
+                        name='first_name'
+                        placeholder='Enter First Name'
+                        value={values.first_name}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Last Name 
+                    <input
+                        type='text'
+                        name='last_name'
+                        placeholder='Enter Last Name'
+                        value={values.last_name}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Email 
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Enter Email'
+                        value={values.email}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Password 
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='Type a Password'
+                        value={values.password}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+
+            <div className='form checkbox'>
+                <label>Terms of Service 
+                    <input
+                        type='checkbox'
+                        name='terms'
+                        checked={values.terms}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
         </form>
     )
 }
